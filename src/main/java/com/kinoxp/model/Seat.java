267ab4c1079b856ca5520c906056628823d664c3
Gotze <thorholby@gmail.com>
2026-03-06 11:20:10 +0100

    Add Seat model class

---
+++ src/main/java/com/kinoxp/model/Seat.java
@@ -0,0 +1,47 @@
+package com.kinoxp.model;
+
+public class Seat {
+    private int seatId;
+    private Theater theater;
+    private int seatNumber;
+    private int rowNumber;
+
+    public Seat(int seatId, Theater theater, int seatNumber, int rowNumber) {
+        this.seatId = seatId;
+        this.theater = theater;
+        this.seatNumber = seatNumber;
+        this.rowNumber = rowNumber;
+    }
+
+    public int getSeatId() {
+        return seatId;
+    }
+
+    public void setSeatId(int seatId) {
+        this.seatId = seatId;
+    }
+
+    public Theater getTheater() {
+        return theater;
+    }
+
+    public void setTheater(Theater theater) {
+        this.theater = theater;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public int getRowNumber() {
+        return rowNumber;
+    }
+
+    public void setRowNumber(int rowNumber) {
+        this.rowNumber = rowNumber;
+    }
+}
