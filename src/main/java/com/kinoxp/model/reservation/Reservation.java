ae3ed28757310da2fd740bb30832ec747f0db957
Junes El-Sayed <juneselsayed03@hotmail.com>
2026-03-06 11:15:09 +0100

    Ups

--- src/main/java/com/kinoxp/model/reservation/Reservation.java
+++ src/main/java/com/kinoxp/model/reservation/Reservation.java
@@ -15,4 +15,81 @@ public class Reservation {
     private double totalPrice;
     private Status status;
     private List<Ticket> tickets;
+
+        public Reservation(int reservationID, User user, Showing showing, int rowNumber,
+                            LocalDateTime created, double totalPrice, Status status, List<Ticket> tickets) {
+            this.reservationID = reservationID;
+            this.user = user;
+            this.showing = showing;
+            this.rowNumber = rowNumber;
+            this.created = created;
+            this.totalPrice = totalPrice;
+            this.status = status;
+            this.tickets = tickets;
+        }
+
+    public int getReservationID() {
+        return reservationID;
+    }
+
+    public void setReservationID(int reservationID) {
+        this.reservationID = reservationID;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Showing getShowing() {
+        return showing;
+    }
+
+    public void setShowing(Showing showing) {
+        this.showing = showing;
+    }
+
+    public int getRowNumber() {
+        return rowNumber;
+    }
+
+    public void setRowNumber(int rowNumber) {
+        this.rowNumber = rowNumber;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
 }
+
