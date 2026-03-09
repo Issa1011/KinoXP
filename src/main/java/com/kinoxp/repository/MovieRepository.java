baa1a5a6a04d7bbe672d349882ec01ae59e50ae2
Gotze <thorholby@gmail.com>
2026-03-09 20:14:57 +0100

    Revert MovieRepository changes

--- src/main/java/com/kinoxp/repository/MovieRepository.java
+++ src/main/java/com/kinoxp/repository/MovieRepository.java
@@ -1,7 +1,16 @@
 package com.kinoxp.repository;
 
+import com.kinoxp.model.movie.Genre;
+import com.kinoxp.model.movie.Language;
 import com.kinoxp.model.movie.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface MovieRepository extends JpaRepository<Movie, Long>, JpaSpecificationExecutor<Movie> {}
+import java.util.List;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+    List<Movie> findByGenre(Genre genre);
+
+    List<Movie> findByLanguage(Language language);
+
+    List<Movie> findByTitleContainingIgnoreCase(String title);
+}
